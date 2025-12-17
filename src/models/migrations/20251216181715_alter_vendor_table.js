29f914feca14ed36365b651500aa3649dab5dbcc
mustaalikanchwala <kanchwalamustaali@gmail.com>
2025-12-17 17:54:48 +0530

    Alter vendor table

---
+++ src/models/migrations/20251216181715_alter_vendor_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  return await knex.schema.withSchema()
+  .alterTable('vendors',function(table){
+    
+  })
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
