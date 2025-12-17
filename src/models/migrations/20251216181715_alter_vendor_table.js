ff51eb4be70fe8ca5306117e28dd6d1a4f1005f4
mustaalikanchwala <kanchwalamustaali@gmail.com>
2025-12-17 20:39:01 +0530

    Alter table Vendor

--- src/models/migrations/20251216181715_alter_vendor_table.js
+++ src/models/migrations/20251216181715_alter_vendor_table.js
@@ -5,7 +5,12 @@
 exports.up = async function(knex) {
   return await knex.schema.withSchema()
   .alterTable('vendors',function(table){
-    
+      table.dropIndex('email');
+      table.dropIndex('is_active');
+      table.dropIndex(['is_active', 'vendor_name']);
+
+      table.dropColumn('password');
+      table.dropColumn('is_active');
   })
 
 };
@@ -15,5 +20,12 @@ exports.up = async function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.withSchema(PUBLIC_SCHEMA)
+    .alterTable('vendors', function(table) {
+      table.string('password', 255).nullable();
+      table.boolean('is_active').defaultTo(true).notNullable();
+      
+      table.index('is_active');
+      table.index(['is_active', 'vendor_name']);
+    });
 };
