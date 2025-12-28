ce603bd5a6c20a48c2fd90b75aad3b44a00cea33
KartikGS1211 <kartiksarode05@gmail.com>
2025-12-29 00:03:37 +0530

    made changes in the item status

--- src/models/libs/dbConstants.js
+++ src/models/libs/dbConstants.js
@@ -1,44 +1,50 @@
-const PUBLIC_SCHEMA ='public'
+const PUBLIC_SCHEMA = "public";
 const FIELD_TYPE = {
-    STRING: "string",
-    INTEGER: "integer",
-    BIGINT: "bigint",
-    DOUBLE: "double",
-    BOOLEAN: "boolean",
-    ENUM: "enum",
-    JSONB: "jsonb",
-    DATE: "date",
-    DATETIME: "date-time"
-}
-
-
-const TABLE_DEFAULTS = {
-    COLUMNS: {
-        CREATED_BY: {
-            KEY: "created_by",
-            TYPE: FIELD_TYPE.INTEGER
-        },
-        LAST_MODIFIED_BY: {
-            KEY: "last_modified_by",
-            TYPE: FIELD_TYPE.INTEGER
-        },
-        IS_DELETED: {
-            KEY: "is_deleted",
-            TYPE: FIELD_TYPE.BOOLEAN
-        },
-        CREATED_AT: {
-            KEY: "created_at",
-            TYPE: FIELD_TYPE.DATETIME
-        },
-        UPDATED_AT: {
-            KEY: "updated_at",
-            TYPE: FIELD_TYPE.DATETIME
-        },
-        IS_ACTIVE: {
-            KEY: "is_active",
-            TYPE: FIELD_TYPE.BOOLEAN
-        }
-    }
+  STRING: "string",
+  INTEGER: "integer",
+  BIGINT: "bigint",
+  DOUBLE: "double",
+  BOOLEAN: "boolean",
+  ENUM: "enum",
+  JSONB: "jsonb",
+  DATE: "date",
+  DATETIME: "date-time",
 };
 
-module.exports={PUBLIC_SCHEMA,TABLE_DEFAULTS}
+const TABLE_DEFAULTS = {
+  COLUMNS: {
+    CREATED_BY: {
+      KEY: "created_by",
+      TYPE: FIELD_TYPE.INTEGER,
+    },
+    LAST_MODIFIED_BY: {
+      KEY: "last_modified_by",
+      TYPE: FIELD_TYPE.INTEGER,
+    },
+    IS_DELETED: {
+      KEY: "is_deleted",
+      TYPE: FIELD_TYPE.BOOLEAN,
+    },
+    CREATED_AT: {
+      KEY: "created_at",
+      TYPE: FIELD_TYPE.DATETIME,
+    },
+    UPDATED_AT: {
+      KEY: "updated_at",
+      TYPE: FIELD_TYPE.DATETIME,
+    },
+    IS_ACTIVE: {
+      KEY: "is_active",
+      TYPE: FIELD_TYPE.BOOLEAN,
+    },
+  },
+};
+
+const ITEM_STATUS = Object.freeze({
+  SOLD: "sold",
+  OUTWARD: "outward",
+  DAMAGED: "damaged",
+  RETURNED: "returned",
+});
+
+module.exports = { PUBLIC_SCHEMA, TABLE_DEFAULTS, ITEM_STATUS };
