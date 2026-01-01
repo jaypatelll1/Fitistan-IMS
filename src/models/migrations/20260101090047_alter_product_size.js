e8734cd815a4bf94647649c8f170def3318cdeae
dakshmiyani <dakshmiyani111@gmail.com>
2026-01-01 14:38:20 +0530

    product size added

---
+++ src/models/migrations/20260101090047_alter_product_size.js
@@ -0,0 +1,23 @@
+const { PUBLIC_SCHEMA } = require("../libs/dbConstants");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.withSchema(PUBLIC_SCHEMA).alterTable("products", (table)=>{
+        table.string("size",100)
+        .nullable()
+    })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.withSchema(PUBLIC_SCHEMA).alterTable("products", (table)=>{
+        table.dropColumn("size");
+    })
+};
