2ed6ac5d41b3a0594829444a26c49748c58f584c
tikmaram prajapati <tikmaramprajapati10b@gmail.com>
2026-01-02 15:03:03 +0530

    feat: Add product model for data management and Joi schemas for product validation.

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -155,15 +155,18 @@ class ProductModel extends BaseModel {
         return null;
       }
 
-      const [updatedProduct] = await qb(this.tableName)
+      const updatedRows = await qb(this.tableName)
         .where(this.whereStatement({ product_id }))
         .update({
           ...data,
           [TABLE_DEFAULTS.COLUMNS.UPDATED_AT.KEY]: qb.raw("CURRENT_TIMESTAMP"),
-        })
-        .returning(this.getPublicColumns());
+        });
 
-      return updatedProduct || null;
+      if (!updatedRows) {
+        return null;
+      }
+
+      return this.findById(product_id);
     } catch (e) {
       throw new DatabaseError(e);
     }
